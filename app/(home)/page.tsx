cbc209954e8065bb90ce041b2daf73e948166fc8
Valentine Omonya <valomosh254@gmail.com>
2025-03-02 20:43:31 +0300

    Refactor home page structure

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,22 +1,22 @@
 import CurrentPrecipitation from "@/screens/home/components/CurrentPrecipitation";
 import HeroBanner from "@/screens/home/components/HeroBanner";
 import OtherCities from "@/screens/home/widgets/OtherCities";
-import Precipitation from "@/screens/home/widgets/Precipitation";
+import MonthlyIncome from "@/screens/home/widgets/MonthlyIncome";
 import Pressure from "@/screens/home/widgets/Pressure";
 import SummaryTable from "@/screens/home/widgets/SummaryTable";
-import Temperature from "@/screens/home/widgets/Temperature";
+import Expenditure from "@/screens/home/widgets/Expenditure";
 import Trends from "@/screens/home/widgets/Trends";
-import Twilight from "@/screens/home/widgets/Twilight";
+import IncomeAndExpense from "@/screens/home/widgets/IncomeAndExpense";
 import Weekly from "@/screens/home/widgets/Weekly";
-import Wind from "@/screens/home/widgets/Wind";
+import ExpenseSavingsRevenue from "@/screens/home/widgets/ExpenseSavingsRevenue";
 export default function Home() {
   return (
     <main className="grid grid-cols-12 px-4 my-12 max-w-[1200px] ">
       <HeroBanner />
-      <Temperature />
-      <Precipitation />
-      <Twilight />
-      <Wind />
+      <Expenditure />
+      <MonthlyIncome />
+      <IncomeAndExpense />
+      <ExpenseSavingsRevenue />
       <CurrentPrecipitation />
       <Pressure />
       <Weekly />
