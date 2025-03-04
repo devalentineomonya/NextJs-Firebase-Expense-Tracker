111ac21c238105020de3f8ac1430dfe38d69c5ab
Valentine Omonya <valomosh254@gmail.com>
2025-03-04 16:49:21 +0300

    Updated Firebase configuration

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -1,6 +1,7 @@
 import { getApps, getApp, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,5 +17,5 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const firestore = getFirestore(app);
-
-export { auth, firestore, app };
+const storage = getStorage(app);
+export { auth, firestore, app, storage };
