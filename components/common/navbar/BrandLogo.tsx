d0076040d0fa62a48b78bed2b2e5d079332c261e
Valentine Omonya <valomosh254@gmail.com>
2025-03-04 16:49:18 +0300

    Updated BrandLogo component

--- components/common/navbar/BrandLogo.tsx
+++ components/common/navbar/BrandLogo.tsx
@@ -4,7 +4,7 @@ import { NavigationMenuItem } from "@/components/ui/navigation-menu";
 
 const BrandLogo = () => (
   <NavigationMenuItem className="col-span-2 font-grotesk font-medium text-2xl flex items-center gap-x-2">
-    <Image src={Logo} alt="Logo" className="w-20 sm:w-8" />
+    <Image src={Logo} alt="Logo" className="w-12 sm:w-8" />
     <span className="max-sm:hidden">DeExpenser</span>
   </NavigationMenuItem>
 );
