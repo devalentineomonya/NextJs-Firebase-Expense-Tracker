69313b5a969f5cca8bab457facbacb71d8c76d36
Valentine Omonya <valomosh254@gmail.com>
2025-03-04 16:49:25 +0300

    Added correct profile page file

---
+++ app/(user)/profile/page.tsx
@@ -0,0 +1,11 @@
+import ProfileBanner from "@/screens/profile/ProfileBanner";
+
+const ProfilePage = () => {
+  return (
+    <div className="w-full h-full my-12 max-w-[1200px]">
+      <ProfileBanner />
+    </div>
+  );
+};
+
+export default ProfilePage;
