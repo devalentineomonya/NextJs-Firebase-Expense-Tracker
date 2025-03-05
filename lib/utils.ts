1280c12e1b20c64d78a9b48f67dac325ed0a5252
Valentine Omonya <valomosh254@gmail.com>
2025-03-05 05:25:08 +0300

    Refactored utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const exportToCSV = <T extends Record<string, unknown>>(data: T[], fileName: string) => {
+    const csvContent = [
+      Object.keys(data[0] || {}).join(","),
+      ...data.map(item => Object.values(item).join(","))
+    ].join("\n");
+
+    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+    const link = document.createElement("a");
+    const url = URL.createObjectURL(blob);
+
+    link.setAttribute("href", url);
+    link.setAttribute("download", `${fileName}.csv`);
+    link.style.visibility = "hidden";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
