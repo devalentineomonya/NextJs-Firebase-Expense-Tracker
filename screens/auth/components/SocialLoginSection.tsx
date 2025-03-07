833eb9b505286ff4b03cb778860ec8e2726e8212
Valentine Omonya <valomosh254@gmail.com>
2025-03-07 22:32:35 +0300

    � Enhanced social login UI

--- screens/auth/components/SocialLoginSection.tsx
+++ screens/auth/components/SocialLoginSection.tsx
@@ -1,25 +1,37 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import {
   useSignInWithGoogle,
   useSignInWithGithub,
 } from "react-firebase-hooks/auth";
-import { auth } from "@/lib/firebase/config";
+import { auth, firestore } from "@/lib/firebase/config";
+import { toast } from "sonner";
+import useAuthStore from "@/lib/zustand/use-authenticating";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+
+type SearchParams = {
+  redirect?: string;
+};
 
 const SocialButton = ({
   provider,
   iconSrc,
   loading,
+  disabled,
   onClick,
 }: {
   provider: string;
   iconSrc: string;
   loading: boolean;
+  disabled: boolean;
   onClick: () => void;
 }) => {
   return (
     <Button
-      disabled={loading}
+      disabled={disabled}
       variant="outline"
       className="w-full flex gap-2 items-center py-6 px-4 border border-ld"
       onClick={onClick}
@@ -34,9 +46,77 @@ const SocialButton = ({
   );
 };
 
-export const SocialLoginSection = () => {
-  const [signInWithGoogle, , googleLoading] = useSignInWithGoogle(auth);
-  const [signInWithGitHub, , githubLoading] = useSignInWithGithub(auth);
+export const SocialLoginSection = ({
+  searchParams,
+}: {
+  searchParams?: SearchParams;
+}) => {
+  const router = useRouter();
+  const redirectUrl = searchParams?.redirect || "/dashboard";
+  const { isAuthenticating, setAuthState } = useAuthStore();
+
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  const [signInWithGitHub, githubUser, githubLoading, githubError] =
+    useSignInWithGithub(auth);
+  const [toastId, setToastId] = useState<string | number | undefined>(
+    undefined
+  );
+
+  const handleAuth = async (authFunction: () => void) => {
+    setAuthState(true);
+    const loadingToastId = toast.loading("Creating session, redirecting...");
+    setToastId(loadingToastId);
+    authFunction();
+  };
+
+  useEffect(() => {
+    const user = googleUser || githubUser;
+
+    if (user) {
+      const { uid, displayName, email } = user.user;
+
+      if (!uid || !email) {
+        toast.error("Error retrieving user data.");
+        setAuthState(false);
+        return;
+      }
+
+      const userDocRef = doc(firestore, "users", uid);
+
+      const saveUserToFirestore = async () => {
+        try {
+          const userDoc = await getDoc(userDocRef);
+          if (!userDoc.exists()) {
+            await setDoc(userDocRef, {
+              name: displayName || "Anonymous",
+              email,
+              isProfileComplete: false,
+              createdAt: new Date(),
+            });
+          }
+        } catch (error) {
+          console.error("Error saving user data:", error);
+          toast.error("Failed to save user data.");
+        }
+      };
+
+      saveUserToFirestore().then(() => {
+        toast.dismiss(toastId);
+        router.push(redirectUrl);
+        setAuthState(false);
+      });
+    }
+  }, [googleUser, githubUser, router, redirectUrl, setAuthState, toastId]);
+
+  useEffect(() => {
+    if (googleError || githubError) {
+      console.error("Authentication Error:", googleError || githubError);
+      toast.dismiss(toastId);
+      toast.error("Authentication failed. Please try again.");
+      setAuthState(false);
+    }
+  }, [googleError, githubError, setAuthState, toastId]);
 
   return (
     <div className="flex gap-4 my-6">
@@ -44,13 +124,15 @@ export const SocialLoginSection = () => {
         provider="Google"
         iconSrc="/google.svg"
         loading={googleLoading}
-        onClick={() => signInWithGoogle()}
+        disabled={isAuthenticating}
+        onClick={() => handleAuth(signInWithGoogle)}
       />
       <SocialButton
         provider="GitHub"
         iconSrc="/github.svg"
         loading={githubLoading}
-        onClick={() => signInWithGitHub()}
+        disabled={isAuthenticating}
+        onClick={() => handleAuth(signInWithGitHub)}
       />
     </div>
   );
